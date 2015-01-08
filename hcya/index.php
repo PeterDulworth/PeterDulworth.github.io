28a091e789817762b2859859cd0fd469a249b148
Peter Dulworth <pdulworth97@gmail.com>
2015-01-08 14:41:54 -0600

    updated index.php

---
+++ hcya/index.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<title>HCYA INDEX PAGE</title>
+	</head>
+	<body>
+		<h1>HCYA INDEX PAGE</h1>
+		<ul>
+			<li><a href = "https://www.google.com">Link 1</a></li>
+			<li><a hred = "https://www.peterdulworth.com">Link 2</a></li>
+		</ul>
+	</body>
+</html>
